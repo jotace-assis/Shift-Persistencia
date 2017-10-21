d8c4a2fc9295ac94e61a828d45358a68f2a1a473
jotace-assis <jotace.assis@outlook.com>
2017-10-21 16:44:48 -0200

    usando Stetho

--- app/src/main/java/br/com/jotaceassis/shiftpersistencia/UsuarioActivity.java
+++ app/src/main/java/br/com/jotaceassis/shiftpersistencia/UsuarioActivity.java
@@ -1,13 +1,54 @@
 package br.com.jotaceassis.shiftpersistencia;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
 
 public class UsuarioActivity extends AppCompatActivity {
+
+    private SharedPreferences sp;
+
+    private EditText etUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
+
+        sp = getPreferences(MODE_PRIVATE);
+
+        etUsuario = (EditText) findViewById(R.id.etUsuario);
+        etUsuario.setText(sp.getString("usuario", ""));
+
+        etUsuario.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                SharedPreferences.Editor e = sp.edit();
+                e.putString("usuario", editable.toString());
+                e.apply();
+            }
+        });
+    }
+
+    public void conectar(View v) {
+
+    }
+
+    public void limpar(View v) {
+
     }
 }
